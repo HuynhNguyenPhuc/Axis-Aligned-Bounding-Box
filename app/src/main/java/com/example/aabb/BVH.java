5f9123cb244d7246ee467ff26798a6043a7fc4ca
Huỳnh Nguyên Phúc <phucthcsmyloc@gmail.com>
2024-06-28 14:35:09 +0700

    Add SplitStrategy interface for split strategy and add documentations

--- app/src/main/java/com/example/aabb/BVH.java
+++ app/src/main/java/com/example/aabb/BVH.java
@@ -1,13 +1,25 @@
 package com.example.aabb;
 
-import java.util.Arrays;
+import androidx.annotation.NonNull;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Represent a node of the Bounding Volume Hierarchy tree
+ */
 class BVHNode {
     public AABB aabb;
     public int id;
     public BVHNode left;
     public BVHNode right;
 
+    /**
+     * @param aabb: The bounding volume
+     * @param id: The index of this node (-1 for internal node,
+     *          non-negative integer for leaf node)
+     */
     public BVHNode(AABB aabb, int id) {
         this.aabb = aabb;
         this.id = id;
@@ -15,6 +27,9 @@ class BVHNode {
         this.right = null;
     }
 
+    /**
+     * Check if a node is leaf node or not
+     */
     public boolean isLeaf() {
         return left == null && right == null;
     }
@@ -33,11 +48,10 @@ public class BVH {
         this.numAABBs = 0;
     }
 
-    public BVH(BVH other) {
-        this.root = other.root;
-        this.numAABBs = other.numAABBs;
-    }
-
+    /**
+     * Return the string represents the structure of the BVH tree
+     */
+    @NonNull
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder(100000);
@@ -45,6 +59,12 @@ public class BVH {
         return sb.toString();
     }
 
+    /**
+     * Recursively print the structure of the BVH tree
+     * @param sb: The string builder
+     * @param node: The current node we are considering
+     * @param depth: The current depth of this node
+     */
     private void toString(StringBuilder sb, BVHNode node, int depth) {
         if (node == null) {
             return;
@@ -61,6 +81,9 @@ public class BVH {
         }
     }
 
+    /**
+     * Pair class for helping sort AABBs but not shuffle the index of each AABB
+     */
     class Pair{
         public AABB aabb;
         public int id;
@@ -70,6 +93,14 @@ public class BVH {
         }
     }
 
+    /**
+     * Sorting the list of AABBs along the axis 0, 1 or 2
+     * @param aabbs: List of AABBs
+     * @param start: Start index
+     * @param end: End index
+     * @param axis: Axis value (0 for x, 1 for y, 2 for z)
+     * @param indices: The array for keeping track of the indices
+     */
     private void sortAABBs(AABB[] aabbs, int start, int end, int axis, int[] indices) {
         Pair[] pairs = new Pair[end - start];
         for (int i = start; i < end; i++) {
@@ -84,7 +115,12 @@ public class BVH {
         }
     }
 
+    /**
+     * Build the BVH tree from the list of AABBs
+     * @param aabbs: List of AABBs
+     */
     public void build(AABB[] aabbs){
+        numAABBs = aabbs.length;
         int[] indices = new int[aabbs.length];
         for (int i = 0; i < aabbs.length; i++) {
             indices[i] = i;
@@ -92,6 +128,14 @@ public class BVH {
         root = buildRecursive(aabbs, 0, aabbs.length, indices);
     }
 
+    /**
+     * Recursively build the BVH tree
+     * @param aabbs: List of AABBs
+     * @param start: Start index
+     * @param end: End index
+     * @param indices: Index of each AABB
+     * @return: The node of the BVH tree which is built from the AABBs
+     */
     private BVHNode buildRecursive(AABB[] aabbs, int start, int end, int[] indices){
         if (start >= end) return null;
         if (end - start == 1) return new BVHNode(aabbs[start], indices[start]);
@@ -105,9 +149,10 @@ public class BVH {
         /* Sort the AABBs based on the longest extent axis */
         sortAABBs(aabbs, start, end, axis, indices);
 
-        /* Median split strategy */
+        /* Surface area heuristic strategy */
         int splitIndex = strategy("sah", aabbs, start, end);
 
+        /* Continue to build form left and right part */
         BVHNode leftChild = buildRecursive(aabbs, start, splitIndex, indices);
         BVHNode rightChild = buildRecursive(aabbs, splitIndex, end, indices);
 
@@ -118,49 +163,28 @@ public class BVH {
         return node;
     }
 
-    private int strategy(String mode, Object... agrs){
-        switch(mode){
-            case "median":
-                return medianStrategy((int) agrs[0], (int) agrs[1]);
-            case "sah":
-                return sahStrategy((AABB[]) agrs[0], (int) agrs[1], (int) agrs[2]);
-            default:
-                return 0;
-        }
+    /**
+     * Get the split index based on the split strategy
+     * @param mode The strategy you want to split: median, random or surface area heuristic
+     * @param args The necessary arguments for this strategy
+     * @return The value of the split index
+     */
+    private int strategy(String mode, Object... args){
+        Map<String, SplitStrategy> strategy = new HashMap<>();
+        strategy.put("median", new MedianStrategy());
+        strategy.put("random", new RandomStrategy());
+        strategy.put("sah", new SurfaceAreaHeuristicStrategy());
+
+        SplitStrategy helper = strategy.get(mode);
+        if (helper == null) return -1;
+        return helper.strategy(args);
     }
 
-    private int medianStrategy(int start, int end){
-        return start + (end - start) / 2;
-    }
-
-    private int sahStrategy(AABB[] aabbs, int start, int end){
-        int n = end - start;
-        float[] sahCosts = new float[n - 1];
-        AABB combinedAABB = AABB.expand(aabbs, start, end);
-
-        float totalSurfaceArea = combinedAABB.getSurfaceArea();
-
-        for (int i = start + 1; i < end; i++) {
-            AABB leftAABB = AABB.expand(aabbs, start, i);
-            AABB rightAABB = AABB.expand(aabbs, i, end);
-            float leftSurfaceArea = leftAABB.getSurfaceArea();
-            float rightSurfaceArea = rightAABB.getSurfaceArea();
-            sahCosts[i - start - 1] = (i - start) * (leftSurfaceArea / totalSurfaceArea) +
-                    (end - i) * (rightSurfaceArea / totalSurfaceArea);
-        }
-
-        float minCost = Float.POSITIVE_INFINITY;
-        int minIndex = start + 1;
-        for (int i = start + 1; i < end; i++) {
-            if (sahCosts[i - start - 1] < minCost) {
-                minCost = sahCosts[i - start - 1];
-                minIndex = i;
-            }
-        }
-
-        return minIndex;
-    }
-
+    /**
+     * Insert a new AABB into the BVH tree
+     * @param aabb: The new axis-aligned bounding box
+     * @param id: The index of this AABB
+     */
     public void insert(AABB aabb, int id) {
         numAABBs++;
         if (root == null) {
@@ -170,6 +194,12 @@ public class BVH {
         }
     }
 
+    /**
+     * Recursively insert a new AABB into the BVH tree
+     * @param node: The current node we are considering
+     * @param aabb: The new axis-aligned bounding box
+     * @param id: The index of this aabb
+     */
     private void insertRecursive(BVHNode node, AABB aabb, int id) {
         if (node.isLeaf()) {
             node.left = new BVHNode(node.aabb, node.id);
@@ -177,9 +207,11 @@ public class BVH {
             node.aabb = node.aabb.expand(aabb);
             node.id = -1;
         } else {
+            /* Calculate the increase in volume if we add into the left and right child */
             float leftVolumeIncrease = node.left.aabb.expand(aabb).getSurfaceArea() - node.left.aabb.getSurfaceArea();
             float rightVolumeIncrease = node.right.aabb.expand(aabb).getSurfaceArea() - node.right.aabb.getSurfaceArea();
 
+            /* Insert into the branch which has less increase */
             if (leftVolumeIncrease < rightVolumeIncrease){
                 insertRecursive(node.left, aabb, id);
                 node.aabb = node.aabb.expand(node.left.aabb);
@@ -190,12 +222,23 @@ public class BVH {
         }
     }
 
+    /**
+     * Check if a ray intersects with any of the AABBs in the BVH tree
+     * @param r: The ray we are checking
+     * @return: A boolean array with the same length as the number of AABBs
+     */
     public boolean[] checkIntersectWithRay(Ray r) {
         boolean[] intersections = new boolean[numAABBs];
         checkIntersectWithRayRecursive(root, r, intersections);
         return intersections;
     }
 
+    /**
+     * Recursively check if a ray intersects with any of the AABBs in the BVH tree
+     * @param node: The current node we are considering
+     * @param r: The ray we are checking
+     * @param intersections: A boolean array with the same length as the number of AABBs
+     */
     private void checkIntersectWithRayRecursive(BVHNode node, Ray r, boolean[] intersections) {
         if (node == null) return;
         if (node.aabb.checkIntersectWithRay(r)) {
@@ -208,31 +251,35 @@ public class BVH {
         }
     }
 
-    class ArrayWrapper{
-        public int[] array;
-        public ArrayWrapper(int initialSize){
-            array = new int[initialSize];
-        }
+    /**
+     * Get list of AABBs which intersect with the ray
+     * @param r: The ray we are considering
+     * @return: List of AABBs
+     */
+    public int[] getIntersectWithRay(Ray r) {
+        DynamicArray result = new DynamicArray(10);
+        int count = getIntersectWithRayRecursive(root, r, result, 0);
+        return Arrays.copyOf(result.array, count);
     }
 
-    public int[] getIntersectWithRay(Ray r, int initialSize) {
-        ArrayWrapper arrayWrapper = new ArrayWrapper(initialSize);
-        int count = getIntersectWithRayRecursive(root, r, arrayWrapper, 0);
-        int[] result = Arrays.copyOf(arrayWrapper.array, count);
-        return result;
-    }
-
-    private int getIntersectWithRayRecursive(BVHNode node, Ray r, ArrayWrapper arrayWrapper, int index) {
+    /**
+     * Recursively get list of AABBs which intersect with the ray
+     * @param node: The current node we are considering
+     * @param r: The ray we are checking
+     * @param array: The dynamic array for tracking the result
+     * @param index: The current number of AABBs which intersect with the ray
+     */
+    private int getIntersectWithRayRecursive(BVHNode node, Ray r, DynamicArray array, int index) {
         if (node == null) return index;
         if (node.aabb.checkIntersectWithRay(r)) {
             if (node.isLeaf()) {
-                if (index >= arrayWrapper.array.length) {
-                    arrayWrapper.array = Arrays.copyOf(arrayWrapper.array, 2 * index);
+                if (array.overflow(index)) {
+                    array.resize();
                 }
-                arrayWrapper.array[index++] = node.id;
+                array.array[index++] = node.id;
             } else {
-                index = getIntersectWithRayRecursive(node.left, r, arrayWrapper, index);
-                index = getIntersectWithRayRecursive(node.right, r, arrayWrapper, index);
+                index = getIntersectWithRayRecursive(node.left, r, array, index);
+                index = getIntersectWithRayRecursive(node.right, r, array, index);
             }
         }
         return index;
